1829598d9acf9e097f8877c9566ed10d490130d5
DixonCi2 <dixon.anato@ci2.co>
2025-04-13 20:01:18 -0500

    Añadir ejemplos de tipos, operadores y estructuras en Go

--- types.go
+++ types.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 const name string = "John Doe"
 
@@ -62,10 +65,111 @@ func main() {
 	const (
 		r = 10
 		l = 20
-		k // k will have the same value as l
+		n // k will have the same value as l
 	)
 
-	fmt.Println(r + l + k)
+	fmt.Println(r + l + n)
 	fmt.Println(l)
+
+	var myInt int = 65
+	var myFloat float64 = float64(myInt)
+	var myString string = string(myInt)
+	fmt.Println(myFloat)
+	fmt.Println(myString)
+
+	//Operadores aritméticos
+	var j int = 10
+	var k int = 20
+	fmt.Println(j + k)
+	fmt.Println(j - k)
+	fmt.Println(j * k)
+	fmt.Println(j / k)
+	fmt.Println(j % k)
+
+	strconv.Itoa(42)
+	strconv.ParseInt("100", 10, 64)
+	strconv.ParseFloat("100.5", 64)
+	strconv.ParseBool("true")
+
+	//Operadores de comparación
+	fmt.Println(j == k)
+	fmt.Println(j != k)
+	fmt.Println(j > k)
+	fmt.Println(j < k)
+	fmt.Println(j >= k)
+	fmt.Println(j <= k)
+
+	//Arrays
+	var arr [5]int
+	arr[0] = 1
+	arr[1] = 2
+	arr[2] = 3
+	arr[3] = 4
+	arr[4] = 5
+	fmt.Println(arr)
+	//Array initialization
+	arr2 := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(arr2)
+	//Array length
+	fmt.Println(len(arr2))
+	//Array slice
+	arr3 := arr2[1:4]
+	fmt.Println(arr3)
+	//Array slice with step
+	arr4 := arr2[1:4:5]
+	fmt.Println(arr4)
+	//Array slice with step and length
+
+	myArray := [4]string{"apple", "banana", "cherry", "date"}
+	mySlice := myArray[1:3]
+	fmt.Println(mySlice)      // Output: [banana cherry]
+	fmt.Println(len(mySlice)) // Output: 2
+
+	// := Short variable declaration, sirve para declarar e incializar una variable en una sola linea
+	// y Go infiere automáticamente el tipo de esa variable según el valor que le des.
+
+	//declaracion tradicional
+	/*
+		var x int = 10  Declarás x, indicás su tipo (int) y le das un valor (10).
+
+		Forma con := (declaración corta):
+		x := 10  Declarás x, Go infiere que es un int y le das un valor (10).
+
+		Reglas:
+		1. Solo funciona dentro de funciones (no se puede usar fuera del main() o de una función).
+		2. Al menos una variable del lado izquierdo debe ser nueva (si no, da error).
+		3. El tipo se infiere automáticamente por el compilador según el valor a la derecha.
+
+	*/
+
+	/*
+		Slices es una vista dinamica sobre una secuencia de elementos en un array, no tienen un tamaño fijo
+		internamente apuntan a un array, pero no tienen un tamaño fijo, por lo que pueden crecer y decrecer
+
+		tienen 3 atributos:
+		1. Pointer: referencia al primer elemento del array subyacente
+		2. Length: longitud del slice
+		3. Capacity: capacidad del slice, que es la longitud del array subyacente
+		4. El slice es un puntero a un array, por lo que si modificas el slice, modificas el array
+	*/
+
+	var slice []string
+
+	slice = make([]string, 5)
+	slice[0] = "apple"
+	slice[1] = "banana"
+	fmt.Println(len(slice))
+
+	type Person struct {
+		Name string
+		Age  int
+	}
+
+	var p Person = Person{Name: "John", Age: 30}
+	//fmt.Println(p.Name)
+	//fmt.Println(p.Age)
+	//p.Name = "Dixon"
+	//p.Age = 30
+	fmt.Println(p)
 
 }
